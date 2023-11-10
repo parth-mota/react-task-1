6206d654b151576abcc1a3afd710ce00792a056e
parth-mota <135701981+parth-mota@users.noreply.github.com>
2023-11-10 11:49:01 +0530

    app.js

---
+++ src/app.js
@@ -0,0 +1,50 @@
+import React from "react";
+import { useState, useEffect } from "react";
+import "./styles.css";
+
+
+function App() {
+  const [text, setText] = useState('');
+
+  const [wordCount, setWordCount] = useState(0);
+
+  const [charCount, setCharCount] = useState(0);
+
+  const changeHandler = (event) => {
+    setText(event.target.value);
+  };
+
+  useEffect(() => {
+    const words = text.split(' ');
+
+    let wordCount = 0;
+    words.forEach((word) => {
+      if (word.trim() !== '') {
+        wordCount++;
+      }
+    });
+    setWordCount(wordCount);
+
+    setCharCount(text.length);
+  }, [text]);
+
+  return (
+    <div className='container'>
+      <h2>Responsive Paragraph Word Counter</h2>
+      <div>
+        <textarea
+          value={text}
+          onChange={changeHandler}
+          placeholder='Input text here'
+        ></textarea>
+
+        <div>
+          <p className='word-count'>Word Count: {wordCount}</p>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+
+export default App;
